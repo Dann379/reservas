b96d0c74f6220a604bc233264dcff04fa49b37df
Dann379 <orange_555321@hotmail.com>
2026-07-13 12:02:38 -0600

    feat: agrega detalle de paquete por ruta con validación de existencia

--- reservas/src/main/java/com/photobooth/reservas/HelloController.java
+++ reservas/src/main/java/com/photobooth/reservas/HelloController.java
@@ -3,13 +3,14 @@ package com.photobooth.reservas;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
 
 @Controller
 public class HelloController {
-
+    private List <String> paquetes = List.of("Basico", "Premium", "Boda", "XV Años");
     @GetMapping("/hola")
-
     public String saludar(Model model){
         model.addAttribute("nombre", "dann");
         return "hola";
@@ -24,10 +25,17 @@ public class HelloController {
     @GetMapping("/paquetes")
     public String paquetes(Model model) {
         model.addAttribute("mensaje", "Nuestros Paquetes");
-        List<String> paquetes = List.of("Basico", "Premium", "Boda", "XV Años");
         model.addAttribute("paquetes", paquetes);
         return "paquetes";
     }
-
+    @GetMapping("/paquetes/{nombre}")
+    public String detalle(@PathVariable String nombre, Model model){
+        if (!paquetes.contains(nombre)) {          // ¿existe en la lista? El ! lo niega
+            model.addAttribute("mensaje", "No existe el paquete " + nombre);
+        } else {
+            model.addAttribute("paquete", nombre); // solo llega al model si SÍ existe
+        }
+        return "detalle";
+    }
 }
 
